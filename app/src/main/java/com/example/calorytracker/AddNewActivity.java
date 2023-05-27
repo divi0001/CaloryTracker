58d4b9c9c34e726fa6cb4b450c4a42a6a25260bb
Emily <stu227812@mail.uni-kiel.de>
2023-05-27 18:18:12 +0200

    finished the design of addnewactivity for now

--- app/src/main/java/com/example/calorytracker/AddNewActivity.java
+++ app/src/main/java/com/example/calorytracker/AddNewActivity.java
@@ -3,12 +3,22 @@ package com.example.calorytracker;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.EditText;
+import android.widget.Spinner;
 
 public class AddNewActivity extends AppCompatActivity {
+
+    private EditText editNameOfFood, editKCalPerUnit, editUnit, editNumOfUnits;
+    private Spinner spUnit, spExistingFood, spDay, spMonth, spYear, spHour, spMin;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_new);
+
+
+
+
     }
 }
